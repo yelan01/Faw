fc5b2de29f425bc83ce01399fdec0395869b40f1
yelan01 <161634843+yelan01@users.noreply.github.com>
2026-01-18 23:53:39 +0800

    Update com.py

--- com.py
+++ com.py
@@ -26,9 +26,22 @@ source ~/autoware/install/setup.bash
 
 
 
-//////////MPC的
+//////////MPC的檔案錄製
 ros2 run mpc_io_logger trigger_logger_mpc --algo mpc --truncate --dump-path /tmp/mpc_log.jsonl
 
-////pp的
+////pp的檔案錄製
 ros2 run mpc_io_logger trigger_logger_pp --truncate --dump_path /tmp/pp_log.jsonl
 
+
+//////mpc的權重 這兩個檔案要同時改
+/home/ye/autoware/src/universe/autoware_universe/control/autoware_trajectory_follower_node/param/lateral
+/home/ye/autoware/src/launcher/autoware_launch/autoware_launch/config/control/trajectory_follower/lateral
+
+ //////然後那兩個該死的檔案改完之後計的重新編譯              
+cd ~/autoware
+source /opt/ros/humble/setup.bash
+colcon build --packages-select autoware_launch --symlink-install
+source install/setup.bash
+
+
+               
